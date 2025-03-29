650f6e2b31f09b79d4ab30abecac74e431c057ae
Bruce Green <bruce_green@trimble.com>
2025-03-29 06:53:45 -0500

    2025-03-29-06

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: "/dropzone/",
 })
